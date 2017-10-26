6c8cb14148ca66278fd1ca75e5717c5c9dd6180b
helinjiang <me@helinjiang.com>
2017-10-26 17:19:31 +0800

    refactor: 重构了主逻辑，废除了不必要的 value-checker 类

--- src/index.js
+++ src/index.js
@@ -1,49 +1,106 @@
 const checkByChai = require('./assertion');
 
-/**
- * 获取校验结果
- * @param valueChecker
- * @return {{retCode: number}}
- */
-module.exports = function (valueChecker, styleName = 'expect') {
-  // 校验结果的数组
-  let result = [];
+class CheckByChai {
+  constructor(name, value, rules = {}, opts = {}) {
+    /**
+     * 该检查项的名字
+     * @type {String}
+     */
+    this.name = name;
+    this.value = value;
+    this.rules = rules;
+    this.opts = opts;
 
-  // 校验类型
-  if (typeof valueChecker.rules.type === 'string') {
-    let checkTypeResult = checkByChai.runWithExpect(function (expect) {
-      expect(valueChecker.value).to.be.a(valueChecker.rules.type);
-    });
+    this.result = [];
 
-    // console.log(checkTypeResult);
+    // /**
+    //  * 校验的成功结果展示
+    //  * @type {String | Function}
+    //  */
+    // this.msgOk = '';
+    //
+    // /**
+    //  * 校验的错误结果展示
+    //  * @type {String | Function}
+    //  */
+    // this.msgError = '';
 
-    result.push({
-      rule: `该值的类型必须为${valueChecker.rules.type}`,
-      isValid: !checkTypeResult,
-      message: checkTypeResult && checkTypeResult.message || '',
-      assertionError: checkTypeResult
+    // /**
+    //  * 是否是必须的
+    //  * @type {Boolean}
+    //  */
+    // this.isRequired = false;
+    //
+    // /**
+    //  * 字段类型校验
+    //  * @type {string}
+    //  */
+    // this.type = rules.type;
+    //
+    // /**
+    //  * 值必须是其中的一项
+    //  * @type {Array}
+    //  */
+    // this.oneOf = [];
+    //
+    // /**
+    //  * 自定义的校验函数
+    //  */
+    // this.checker = rules.checker;
+
+  }
+
+  check() {
+    this._checkType();
+    this._checkChecker();
+
+    return {
+      retCode: 0,
+      result: this.result
+    };
+  }
+
+  /**
+   * 校验值的类型
+   * @private
+   */
+  _checkType() {
+    if (typeof this.rules.type === 'string') {
+      let checkResult = checkByChai.runWithExpect((function (expect) {
+        expect(this.value).to.be.a(this.rules.type);
+      }).bind(this));
+
+      // console.log(checkResult);
+
+      this._addResult(`该值的类型必须为${this.rules.type}`, checkResult);
+    }
+  }
+
+  /**
+   * 校验自定义的checker
+   * @private
+   */
+  _checkChecker() {
+    if (typeof this.rules.checker === 'function') {
+      let checkResult = checkByChai.runWithExpect((function (expect) {
+        this.rules.checker.call(this, expect);
+      }).bind(this));
+
+      // console.log(checkResult);
+
+      this._addResult(`自定义校验器`, checkResult);
+    }
+  }
+
+  _addResult(rule, checkResult) {
+    this.result.push({
+      rule: rule,
+      isValid: !checkResult,
+      message: checkResult && checkResult.message || '',
+      assertionError: checkResult
     });
   }
 
-  // 校验自定义的checker
-  if (typeof valueChecker.rules.checker === 'function') {
-    let checkCheckerResult = checkByChai.runWithExpect(function (expect) {
-      valueChecker.rules.checker.call(valueChecker, expect);
-    });
+}
 
-    // console.log(checkCheckerResult);
-
-    result.push({
-      rule: `自定义校验器`,
-      isValid: !checkCheckerResult,
-      message: checkCheckerResult && checkCheckerResult.message || '',
-      assertionError: checkCheckerResult
-    });
-  }
-
-  return {
-    retCode: 0,
-    result: result
-  };
-};
-
+module.exports = CheckByChai;
